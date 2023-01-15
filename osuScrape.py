defe68bbbb1f57c6ce57fe66269db7744225cb59
Fan-Yang <fan.yang284@gmail.com>
2023-01-14 22:38:00 -0500

    reformat code to accept user id as command line input

--- osuScrape.py
+++ osuScrape.py
@@ -2,28 +2,44 @@ from bs4 import BeautifulSoup
 import pandas
 from selenium import webdriver
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+import sys
 
-plays = []
-accuracy = []
-mods = []
-pp = []
+def getTopPlays(id):
+	driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-driver.get("https://osu.ppy.sh/users/11403312")
+	plays = []
+	accuracy = []
+	mods = []
+	pp = []
 
-content = driver.page_source
-soup = BeautifulSoup(content)
+	driver.get(f'https://osu.ppy.sh/users/{id}')
 
-playLists = soup.findAll('div', attrs={'class':'play-detail-list u-relative'})
+	content = driver.page_source
+	soup = BeautifulSoup(content)
 
-topPlays = playLists[1];
+	playLists = soup.findAll('div', attrs={'class':'play-detail-list u-relative'})
 
-name = [t.getText() for t in topPlays.findAll('a', attrs={'class': 'play-detail__title'})]
-acc = [t.getText() for t in topPlays.findAll('span', attrs={'class': 'play-detail__accuracy'})]
-mods = [t.get('data-acronym') for t in topPlays.findAll('div', attrs={'class': 'mod'})]
-pp = [t.getText() for t in topPlays.findAll('div', attrs={'class': 'play-detail__pp'})]
+	topPlays = playLists[1];
 
-print(name)
-print(acc)
-print(mods)
-print(pp)
+	name = [t.getText() for t in topPlays.findAll('a', attrs={'class': 'play-detail__title'})]
+	acc = [t.getText() for t in topPlays.findAll('span', attrs={'class': 'play-detail__accuracy'})]
+	mods = [t.get('data-acronym') for t in topPlays.findAll('div', attrs={'class': 'mod'})]
+	pp = [t.getText() for t in topPlays.findAll('div', attrs={'class': 'play-detail__pp'})]
+
+	print(name)
+	print(acc)
+	print(mods)
+	print(pp)
+
+
+def main(player_id):
+	getTopPlays(player_id)
+
+if __name__ == "__main__":
+	args = sys.argv
+
+	if len(args) != 2:
+		print(f'Wrong number of arguments: Expected 1, received {len(args)-1}')
+	else:
+		main(args[1])
+
