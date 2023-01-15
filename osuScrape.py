4d47ab218a5c35a72ff2c633896402af1312764b
Fan-Yang <fan.yang284@gmail.com>
2023-01-14 22:14:12 -0500

    get names and accs

--- osuScrape.py
+++ osuScrape.py
@@ -13,5 +13,21 @@ driver.get("https://osu.ppy.sh/users/11403312")
 
 content = driver.page_source
 soup = BeautifulSoup(content)
-for i in soup.findAll('a', href=True, attrs={'class':'play-detail-list u-relative'}):
-    name=a.find('div', attrs={'class':})
+
+playLists = soup.findAll('div', attrs={'class':'play-detail-list u-relative'})
+
+topPlays = playLists[1];
+
+print(topPlays)
+
+name = topPlays.findAll('a', attrs={'class': 'play-detail__title'})
+acc = topPlays.findAll('span', attrs={'class': 'play-detail__accuracy'})
+
+print(name)
+print(acc)
+
+"""
+for div in soup.findAll('div', attrs={'class':'play-detail-list u-relative'}):
+	name = div.find('div', attrs={'class': 'play-detail__title'})
+	acc = div.find('div', attrs={'class': 'play-detail__accuracy'})
+"""
